7dc92cb240b5a3e2f63a14b3f25ac035fccd816e
Zhengjian Kang <zk299@nyu.edu>
2023-02-22 12:22:15 -0500

    Update and rename react_interview.js to react_interview_001_100.js

--- react_interview_001_100.js
+++ react_interview_001_100.js
@@ -263,7 +263,46 @@ You can think of these as the isomorphic or universal helpers that you need to b
 The react-dom package contains ReactDOM.render(), and in react-dom/server we have server-side rendering support with 
 ReactDOMServer.renderToString() and ReactDOMServer.renderToStaticMarkup().
 
+--------------------------------------
+100. How to re-render the view when the browser is resized?
+You can listen to the resize event in componentDidMount() and then update the dimensions (width and height). 
+You should remove the listener in componentWillUnmount() method.
 
+class WindowDimensions extends React.Component {
+  constructor(props) {
+    super(props);
+    this.updateDimensions = this.updateDimensions.bind(this);
+  }
+
+  componentWillMount() {
+    this.updateDimensions();
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
+  updateDimensions() {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }
+
+  render() {
+    return (
+      <span>
+        {this.state.width} x {this.state.height}
+      </span>
+    );
+  }
+}
+
+-------------------------------------
 
 
 
