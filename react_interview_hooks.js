7d88405725f1ba377146b6a73a6571224cfbe10e
Zhengjian Kang <zk299@nyu.edu>
2023-02-22 18:19:10 -0500

    Update react_interview_hooks.js

--- react_interview_hooks.js
+++ react_interview_hooks.js
@@ -5,16 +5,22 @@ export function Counter() {
   [count, setCount] = useState(0)
   
   function adjustCount(amount) {
-    setCount((prevCount) => (preCount + amount));
+    setCount((prevCount) => {
+      return preCount + amount;
+    });
   }
   
-  return
+  return (
     <div>
       <button onClick={() -> adjustCount(-1)}>-</button>
       <span></span>
       <button onClick={() -> adjustCount(1)>+</button>
     </div>
+  )
 }
 
-// 
+// useRef例子
+// ref will not cause the component to re-update when it get changed, will never cause our component to rerendered
+
+
 
