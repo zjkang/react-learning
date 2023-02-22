979ff69e28cb95e42ae2086a4c4c59140b17b1b2
Zhengjian Kang <zk299@nyu.edu>
2023-02-22 17:59:18 -0500

    Create react_interview_redux.js

---
+++ react_interview_redux.js
@@ -0,0 +1,16 @@
+152. What is flux?
+  
+Flux is an application design paradigm used as a replacement for the more traditional MVC pattern. 
+It is not a framework or a library but a new kind of architecture that complements React and 
+the concept of Unidirectional Data Flow. Facebook uses this pattern internally when working with React.
+
+The workflow between dispatcher, stores and views components with distinct inputs and outputs as follows:
+
+---------------------------
+153. What is Redux?
+
+Redux is a predictable state container for JavaScript apps based on the Flux design pattern. 
+Redux can be used together with React, or with any other view library
+
+---------------------------
+153. What are the core principles of Redux?
