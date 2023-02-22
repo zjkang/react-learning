66e2a7bceff21018ed66cef5a1c551edbaaf0b6d
Zhengjian Kang <zk299@nyu.edu>
2023-02-22 17:18:13 -0500

    Update react_interview_101_200.js

--- react_interview_101_200.js
+++ react_interview_101_200.js
@@ -54,6 +54,55 @@ this.setState((prevState) => ({
 }));
 
 -----------------------------------
+127. How to make AJAX call and in which component lifecycle methods should I make an AJAX call?
+You can use AJAX libraries such as Axios, jQuery AJAX, and the browser built-in fetch. 
+You should fetch data in the componentDidMount() lifecycle method. 
+This is so you can use setState() to update your component when the data is retrieved.
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: [],
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.example.com/items")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            employees: result.employees,
+          });
+        },
+        (error) => {
+          this.setState({ error });
+        }
+      );
+  }
+
+  render() {
+    const { error, employees } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else {
+      return (
+        <ul>
+          {employees.map((employee) => (
+            <li key={employee.name}>
+              {employee.name}-{employee.experience}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
+
+
+
 
 
 
