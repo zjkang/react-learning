3e1fdcba77abc209fdf448aa1653abc137f65208
Zhengjian Kang <zk299@nyu.edu>
2023-02-22 19:16:39 -0500

    Update react_interview_redux.js

--- react_interview_redux.js
+++ react_interview_redux.js
@@ -14,3 +14,17 @@ Redux can be used together with React, or with any other view library
 
 ---------------------------
 153. What are the core principles of Redux?
+[1] Single source of truth: The state of your whole application is stored in an object tree within a single store. 
+The single state tree makes it easier to keep track of changes over time and debug or inspect the application.
+[2] State is read-only: The only way to change the state is to emit an action, an object describing what happened. 
+This ensures that neither the views nor the network callbacks will ever write directly to the state.
+[3] Changes are made with pure functions: To specify how the state tree is transformed by actions, you write reducers. 
+Reducers are just pure functions that take the previous state and an action as parameters, and return the next state.
+
+------------------------------
+157 Can I dispatch an action in reducer?
+Dispatching an action within a reducer is an anti-pattern. 
+Your reducer should be without side effects, simply digesting the action payload and returning a new state object. 
+Adding listeners and dispatching actions within the reducer can lead to chained actions and other side effects.
+
+
