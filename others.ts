50590d6a0bf9d5fd48d496a25936982a1c71eef8
Zhengjian Kang <zk299@nyu.edu>
2023-02-23 10:23:43 -0500

    Update others.ts

--- others.ts
+++ others.ts
@@ -1,5 +1,38 @@
 https://github.com/scps960740
 
+difference between react & angular
+https://juejin.cn/post/6950301479412432903
+
+框架与库
+Angular是一个框架，而React是一个库
+
+
+数据更新
+Angular
+
+数据更新检查：
+
+在 Angular 中有一个 Zone.js 负责监听需要视图变化的事件触发
+每一个组件都都它自己的检测器(detector)，用于负责检查其自身模板上绑定的变量。
+将旧值跟新值进行比较，不相等就说明检测到变化，更新对应视图
+
+虚拟 DOM 如何运行？
+
+创建阶段：首先依据 JSX 和基础数据创建出来虚拟 DOM，它反映了真实的 DOM 树的结构。然后由虚拟 DOM 树创建出真实 DOM 树，真实的 DOM 树生成完后，再触发渲染流水线往屏幕输出页面。
+更新阶段：如果数据发生了改变，那么就需要根据新的数据创建一个新的虚拟 DOM 树；然后 React 比较两个树，找出变化的地方，并把变化的地方一次性更新到真实的 DOM 树上；最后渲染引擎更新渲染流水线，并生成新的页面。
+
+
+数据绑定和数据流
+
+
+组件通信
+
+父组件向子组件通信
+子组件向父组件通信
+非父子组件通信
+
+
+
 typescript is superset of javascript, static typing
 
 typescript课程
